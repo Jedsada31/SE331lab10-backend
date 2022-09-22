d245e17ebe33602290566ec5255d7388d96494df
Jedsada <Jedsada_kampen@cmu.ac.th>
2022-09-23 01:31:00 +0700

    point 6.2

--- src/main/java/se331/rest/util/LabMapper.java
+++ src/main/java/se331/rest/util/LabMapper.java
@@ -3,6 +3,8 @@ package se331.rest.util;
 import org.mapstruct.factory.Mappers;
 import se331.rest.entity.Event;
 import se331.rest.entity.EventDTO;
+import se331.rest.entity.Organizer;
+import se331.rest.proper.OrganizerDTO;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ public interface LabMapper {
     LabMapper INSTANCE = Mappers.getMapper(LabMapper.class);
     EventDTO getEventDto(Event event);
     List<EventDTO> getEventDto(List<Event> events);
+    OrganizerDTO getOrganizerDTO(Organizer organizer);
+    List<OrganizerDTO> getOrganizerDTO(List<Organizer> organizers);
 }
