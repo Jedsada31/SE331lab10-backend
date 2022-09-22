42d48dd2b344adcb2f7602b0d9a81321d66a2579
Jedsada <Jedsada_kampen@cmu.ac.th>
2022-09-22 18:22:37 +0700

    point 2.2

--- src/main/java/se331/rest/repository/OrganizerRepository.java
+++ src/main/java/se331/rest/repository/OrganizerRepository.java
@@ -1,2 +1,7 @@
-package se331.rest.repository;public interface OrganizerRepository {
+package se331.rest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se331.rest.entity.Organizer;
+
+public interface OrganizerRepository extends JpaRepository<Organizer,Long> {
 }
